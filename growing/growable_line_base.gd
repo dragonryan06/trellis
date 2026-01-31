677ecdbe9cf04657a369881a9026a18358f145f6
dragonryan06 <dragonryan06@gmail.com>
2026-01-31 15:08:08 -0500

    Extract out root logic to base class

---
+++ growing/growable_line_base.gd
@@ -0,0 +1,24 @@
+class_name GrowableLineBase
+extends Line2D
+
+var endpoint_position: Vector2:
+	get:
+		return points[-1]
+	set(value):
+		points[-1] = value
+
+func grow_to(location: Vector2) -> void:
+	assert(is_inside_tree(), "Must be inside tree!")
+	add_point(endpoint_position)
+	var grow_tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
+	grow_tween.tween_property(self, "endpoint_position", location, 1.0)
+	grow_tween.tween_property(self, "width", width + 0.5, 1.0)
+	$GrowthHandle.position = location
+
+func _ready() -> void:
+	GameState.next_turn.connect(_on_next_turn)
+
+func _on_next_turn() -> void:
+	if ($GrowthHandle.was_moved):
+		grow_to($GrowthHandle.position)
+		$GrowthHandle.was_moved = false
--- growing/root.gd
+++ growing/root.gd
@@ -1,30 +1,11 @@
 class_name Root
-extends Line2D
+extends GrowableLineBase
 
 @onready
-var BranchHandleScene = preload("res://growing/branch_handle.tscn")
-
-var root_tip_position: Vector2:
-	get:
-		return points[-1]
-	set(value):
-		points[-1] = value
-
-func grow_to(location: Vector2) -> void:
-	assert(is_inside_tree(), "Must be inside tree!")
-	add_point(root_tip_position)
-	var grow_tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
-	grow_tween.tween_property(self, "root_tip_position", location, 1.0)
-	grow_tween.tween_property(self, "width", width + 0.5, 1.0)
-	$GrowthHandle.position = location
-
-func _ready() -> void:
-	GameState.next_turn.connect(_on_next_turn)
+var BranchHandleScene = preload("res://growing/ui/branch_handle.tscn")
 
 func _on_next_turn() -> void:
-	if ($GrowthHandle.was_moved):
-		grow_to($GrowthHandle.position)
-		$GrowthHandle.was_moved = false
+	super._on_next_turn()
 	
 	for child in get_children():
 		if (!(child is BranchHandle and child.was_moved)):
