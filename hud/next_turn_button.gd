fc99c91385d9392cdb29e70f2fe36ddcbedd910c
dragonryan06 <dragonryan06@gmail.com>
2026-02-14 18:40:11 -0500

    Replace old next turn button

--- hud/next_turn_button.gd
+++ hud/next_turn_button.gd
@@ -1,4 +1,30 @@
-extends TextureButton
+extends Button
+
+var _pressed_played_once := false
+
+## Please use this method instead of setting .disabled
+func disable(state: bool) -> void:
+	disabled = state
+	$AnimatedSprite2D.play(&"disabled" if state else &"default")
 
 func _process(delta: float) -> void:
 	$SubViewportContainer/SubViewport/Icon.rotation += 0.01
+
+func _on_mouse_entered() -> void:
+	$AnimatedSprite2D.play(&"hover")
+
+func _on_mouse_exited() -> void:
+	if (!_pressed_played_once):
+		await $AnimatedSprite2D.animation_looped
+	$AnimatedSprite2D.play(&"default")
+
+func _on_button_down() -> void:
+	_pressed_played_once = false
+	$AnimatedSprite2D.play(&"pressed")
+	await $AnimatedSprite2D.animation_looped
+	_pressed_played_once = true
+
+func _on_button_up() -> void:
+	if (!_pressed_played_once):
+		await $AnimatedSprite2D.animation_looped
+	$AnimatedSprite2D.play(&"hover")
--- meta/test_world.gd
+++ meta/test_world.gd
@@ -3,8 +3,8 @@ extends Node2D
 func _ready() -> void:
 	GameState.hold_state_changed.connect(_on_hold_state_changed)
 
-func _on_next_turn_pressed() -> void:
+func _on_next_turn_button_pressed() -> void:
 	GameState.advance_turn()
 
 func _on_hold_state_changed(state: bool) -> void:
-	$HUD/NextTurn.disabled = state
+	$HUD/NextTurnButton.disable(state)
