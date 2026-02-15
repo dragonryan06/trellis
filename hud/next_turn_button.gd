c06ada30e5d6a80b9fe10afb91b7e026ad53ebaf
dragonryan06 <dragonryan06@gmail.com>
2026-02-14 23:29:51 -0500

    Prototype time of day indicator spinning

--- hud/next_turn_button.gd
+++ hud/next_turn_button.gd
@@ -1,5 +1,12 @@
 extends Button
 
+const ATLAS_REGIONS := {
+	"Dawn": Rect2(0, 0, 128, 128),
+	"Noon": Rect2(128, 0, 128, 128),
+	"Dusk": Rect2(0, 128, 128, 128),
+	"Night": Rect2(128, 128, 128, 128)
+}
+
 var _pressed_played_once := false
 
 ## Please use this method instead of setting .disabled
@@ -8,12 +15,24 @@ func disable(state: bool) -> void:
 	$AnimatedSprite2D.play(&"disabled" if state else &"default")
 
 func _ready() -> void:
-	GameState.next_phase.connect(
-		func(): tooltip_text = "%s %d" % [GameState.phase_name, GameState.turn_number]
-	)
+	GameState.next_phase.connect(_on_next_phase)
 
-func _process(delta: float) -> void:
-	$SubViewportContainer/SubViewport/Icon.rotation += 0.01
+func _on_next_phase() -> void:
+	tooltip_text = "%s %d" % [GameState.phase_name, GameState.turn_number]
+	
+	var sprite = $SubViewportContainer/SubViewport/TimeOfDay
+	var spin = func():
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SPRING)
+		tween.tween_property(sprite, ^"rotation", PI, 1.0)
+		await tween.finished
+		sprite.rotation = 0.0
+	await spin.call()
+	
+	if (GameState.phase_name == "Dawn"):
+		sprite.texture.region = ATLAS_REGIONS["Night"]
+		await spin.call()
+	
+	sprite.texture.region = ATLAS_REGIONS[GameState.phase_name]
 
 func _on_mouse_entered() -> void:
 	$AnimatedSprite2D.play(&"hover")
